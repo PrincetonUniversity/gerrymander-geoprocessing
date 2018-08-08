13d4c1683a7d036eb63d45af063c60bc7123547d
jacobwachspress <jacobmw@princeton.edu>
2018-08-08 16:39:44 -0400

    more bug fixes
    darn cpg

--- num_precincts_sanity_check.py
+++ num_precincts_sanity_check.py
@@ -8,13 +8,19 @@ Created on Wed Aug  8 15:54:21 2018
 import geopandas as gpd
 import pandas as pd
 import csv
+import os
 
 def main():
-    csv_path = 'TODO'
+    csv_path = 'G:/Team Drives/princeton_gerrymandering_project/mapping/VA/Precinct Shapefile Collection/CSV/num_precincts_received_shapefiles.csv'
     compare_election_results_to_shapefiles(csv_path)
     
-def get_num_attributes_from_shp(filename):
-    df = gpd.read_file(filename)
+def get_num_attributes_from_shp(path):
+    # Delete CPG file if it exists
+    cpg_path = ''.join(path.split('.')[:-1]) + '.cpg'
+    if os.path.exists(cpg_path):
+        os.remove(cpg_path)
+        
+    df = gpd.read_file(path)
     return len(df)
 
 def compare_election_results_to_shapefiles(csv_path):
@@ -35,16 +41,14 @@ def compare_election_results_to_shapefiles(csv_path):
 
         # Iterate through each county we are creating a shapefile for
         for i, _ in in_df.iterrows():
-            
-            filename = direc_path + '/' + in_df.at[i, 'Filename']
-            num_precincts = in_df.at[i, 'Num Precincts']
             locality = in_df.at[i, 'Locality']
-            num_precincts_shp = get_num_attributes_from_shp(filename)
-            
+            path = direc_path + '/' + locality +'/' + in_df.at[i, 'Filename']
+            num_precincts = in_df.at[i, 'Num Precincts']
+            num_precincts_shp = get_num_attributes_from_shp(path)
             if (num_precincts != num_precincts_shp):
                 print (locality + ' has ' + str(num_precincts) + ' precincts '\
-                       'in the election results but ' + str(num_precincts_str)\
-                       ' attributes in the shapefile.)
+                       'in the election results but ' + str(num_precincts_shp)\
+                       + ' attributes in the shapefile.')
                 
     # CSV file could not be read in or exported
     except:
