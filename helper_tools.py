ea17cda3a810b86ffddf1a842213c732e264e171
Connor <connormoffatt4@gmail.com>
2018-09-07 22:20:09 -0400

    Add delete_cpg

--- helper_tools.py
+++ helper_tools.py
@@ -893,6 +893,15 @@ def save_shapefile(df, file_path, cols_to_exclude=[]):
     
     return 1
     
+def delete_cpg(path):
+    '''Deletes the CPG with a corresponding SHP. ArcGIS sometimes incorrectly
+    encodes a shapefile and incorrectly saves the CPG. Before running most
+    of the scripts, it is beneficially to ensure an encoding error does throw
+    an error'''
+    
+    cpg_path = ''.join(path.split('.')[:-1]) + '.cpg'
+    if os.path.exists(cpg_path):
+        os.remove(cpg_path)
     
     
     
