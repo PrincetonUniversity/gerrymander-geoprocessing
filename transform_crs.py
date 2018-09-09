1cb9baa3c21ef772445e3e37061ca601ef9ea692
Connor <connormoffatt4@gmail.com>
2018-09-08 23:21:10 -0400

    simplify transform_crs

--- transform_crs.py
+++ transform_crs.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import geopandas as gpd
 import os
-import shapely as shp
-import csv
+import helper_tools as ht
 
 # Get path to our CSV file
 csv_path = "G:/Team Drives/princeton_gerrymandering_project/mapping/VA/Precinct Shapefile Collection/CSV/Misc CSV/county_transform_coordinates.csv"
@@ -10,18 +9,17 @@ csv_path = "G:/Team Drives/princeton_gerrymandering_project/mapping/VA/Precinct 
 # Initial try and except to catch improper csv_path or error exporting the
 # results of the difference
 try:
+    
     # Import Google Drive path
-    with open(csv_path) as f:
-        reader = csv.reader(f)
-        data = [r for r in reader]
-    direc_path = data[0][1]
-
-    # Import table from CSV into pandas dataframe
-    name_list = ['Locality Name', 'File Name', 'CRS']
-    csv_df = pd.read_csv(csv_path, header=1, names=name_list)
+    direc_path = ht.read_one_csv_elem(csv_path)
+    
+    # Import table from CSV into pandas df
+    csv_col = ['Locality Name', 'File Name', 'CRS']
+    csv_list = []
+    csv_df = ht.read_csv_to_df(csv_path, 1, csv_col, csv_list)
 
     # Initialize out_df, which contains the results of the difference
-    new_cols = ['Result', 'Locality Name', 'Notes']
+    new_cols = ['Result', 'Locality Name']
     out_df = pd.DataFrame(columns=new_cols)
     
     # Iterate through each county we are finding the difference for
@@ -38,40 +36,27 @@ try:
             new_crs = csv_df.at[i, 'CRS']
             
             # Read in shapefile
-            cpg_path = ''.join(path_shape.split('.')[:-1]) + '.cpg'
-            if os.path.exists(cpg_path):
-                os.remove(cpg_path)
+            ht.delete_cpg(path_shape)
             df = gpd.read_file(path_shape)
+
+            # transform CRS
+            df = ht.set_CRS(df, new_crs)
             
-            # Check if no coordinate system and we need to treat it as a pdf
-            if df.crs == {}:
-                row = len(out_df)
-                out_df.at[row, 'Result'] = 'FAILURE'
-                out_df.at[row, 'Locality Name'] = local
-                out_df.at[row, 'Notes'] = 'No CRS. Treat as PDF'
+            # save shapefile
+            ht.save_shapefile(df, path_shape)
             
-            # Transform coordinates
-            else:
-                # Transform
-                df = df.to_crs({'init': new_crs})
-                
-                # Save shapefile
-                df.to_file(path_shape)
-            
-                # Place Results in out_df
-                row = len(out_df)
-                out_df.at[row, 'Result'] = 'SUCCESS'
-                out_df.at[row, 'Locality Name'] = csv_df.at[i, 'Locality Name']
-                out_df.at[row, 'Notes'] = 'none'
+            # batch report
+            row = len(out_df)
+            out_df.at[row, 'Result'] = 'SUCCESS'
+            out_df.at[row, 'Locality Name'] = csv_df.at[i, 'Locality Name']
+            out_df.at[row, 'Notes'] = 'none'
         
         # Shapefile creation failed
-        except Exception as e:
-            print(e)
+        except:
             print('ERROR:' + csv_df.at[i, 'Locality'])
             row = len(out_df)
             out_df.at[row, 'Result'] = 'FAILURE'
             out_df.at[row, 'Locality Name'] = csv_df.at[i, 'Locality Name']
-            out_df.at[row, 'Notes'] = e
 
     # Create path to output our results CSV file and output
     csv_out_path = csv_path[:-4] + ' RESULTS.csv'
