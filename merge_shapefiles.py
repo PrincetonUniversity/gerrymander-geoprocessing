8a40772719be96aec24c0c0f5994b800d7d4c545
Connor <connormoffatt4@gmail.com>
2018-09-09 00:19:30 -0400

    simplify merge shapefile

--- merge_shapefiles.py
+++ merge_shapefiles.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import geopandas as gpd
 import os
-import csv
+import helper_tools as ht
 
 # Get path to our CSV file
 csv_path = "G:/Team Drives/princeton_gerrymandering_project/mapping/VA/Precinct Shapefile Collection/CSV/Merge CSV/merge_shapefiles_names_fixed_Aug_21.csv"
@@ -9,24 +9,21 @@ csv_path = "G:/Team Drives/princeton_gerrymandering_project/mapping/VA/Precinct 
 # Initial try and except to catch improper csv_path or error exporting the
 # results of the difference
 try:
+    # Import Google Drive path
+    direc_path = ht.read_one_csv_elem(csv_path)
+    out_path = ht.read_one_csv_elem(csv_path, 1, 1)
+    col_keep = ht.read_one_csv_elem(csv_path, 2, 1)
+    
+    # Import table from CSV into pandas df
+    csv_col = ['Locality Name', 'Path']
+    csv_list = []
+    csv_df = ht.read_csv_to_df(csv_path, 3, csv_col, csv_list)
     
     # Initialize Output Dataframe
     df_final = pd.DataFrame()
-    
-    # Import Google Drive path
-    with open(csv_path) as f:
-        reader = csv.reader(f)
-        data = [r for r in reader]
-    direc_path = data[0][1]
-    out_path = data[1][1]
-    col_keep = data[2][1]
 
-    # Import table from CSV into pandas dataframe
-    name_list = ['Locality Name', 'Path']
-    csv_df = pd.read_csv(csv_path, header=3, names=name_list)
-
-    # Initialize out_df, which contains the results of the difference
-    new_cols = ['Result', 'Locality Name', 'Notes']
+    # Initialize out_df, which contains the batch output
+    new_cols = ['Result', 'Locality Name']
     out_df = pd.DataFrame(columns=new_cols)
 
     # Iterate through each county we are finding the difference for
@@ -36,30 +33,14 @@ try:
         try:
             # Define the locality
             local = csv_df.at[i, 'Locality Name']
+            print(local)
             
             # Get path to load in
-            path_shape = csv_df.at[i, 'Path']
-            
-            if path_shape == 'census_block':
-                filename = local + '_census_block.shp'
-                filename = filename.replace(' ', '_')
-                path_shape = direc_path + '/' + local + '/' + filename
-                                
-            if path_shape == 'precinct':
-                filename = local + '_precincts.shp'
-                path_shape = direc_path + '/' + local + '/' + filename
-                
-            if path_shape == 'precinct_final':
-                filename = local + '_precincts_final.shp'
-                filename = filename.replace(' ', '_')
-                path_shape = direc_path + '/' + local + '/' + filename
-                
-            print(path_shape.split('/')[-1])
+            path_shape = ht.default_path(local, csv_df.at[i, 'Path'], \
+                                         direc_path)
 
             # Read in shapefile
-            cpg_path = ''.join(path_shape.split('.')[:-1]) + '.cpg'
-            if os.path.exists(cpg_path):
-                os.remove(cpg_path)
+            ht.delete_cpg(path_shape)
             df = gpd.read_file(path_shape)
 
             # Append dataframe to overall dataframe
@@ -72,13 +53,11 @@ try:
             out_df.at[row, 'Notes'] = 'none'
         
         # Shapefile creation failed
-        except Exception as e:
-            print(e)
+        except:
             print('ERROR:' + csv_df.at[i, 'Locality'])
             row = len(out_df)
             out_df.at[row, 'Result'] = 'FAILURE'
             out_df.at[row, 'Locality Name'] = csv_df.at[i, 'Locality Name']
-            out_df.at[row, 'Notes'] = e
            
     # Reduce to only columns to keep. Make sure columns are in df_final cols
     if col_keep != 1:
@@ -88,9 +67,9 @@ try:
             if col not in df_final.columns:
                 col_keep.remove(col)
         
-    # Save overall shapefile
+    # Save final shapefile
     df_final = gpd.GeoDataFrame(df_final, geometry='geometry')
-    df_final.to_file(out_path)
+    ht.save_shapefile(df_final, out_path)
 
     # Create path to output our results CSV file and output
     csv_out_path = csv_path[:-4] + ' RESULTS.csv'
