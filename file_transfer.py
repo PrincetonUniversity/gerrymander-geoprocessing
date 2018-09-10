1be5dff2651e492dde9de13bc19a235b1949e465
Connor <connormoffatt4@gmail.com>
2018-09-10 08:56:34 -0400

    Simplify File Transfer

--- file_transfer.py
+++ file_transfer.py
@@ -2,22 +2,7 @@ import os
 import csv
 import pandas as pd
 import shutil
-
-# Princeton Gerrymandering Project
-# Connor Moffatt
-# Python 3
-
-# USER GUIDE: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-# STEP 1: Copy path of folder containing all of the locality folders into the
-# first variable csv_below. Ensure that the path in csv_path uses forward 
-# slashes (/) instead of backslashes (\). DO NOT use replace all because 
-# backslashes are necessary in the rest of the script
-
-# STEP 2: Check all formatting from google sheet was copied correctly into
-# the csv file
-
-# Step 3: Run
+import helper_tools as ht
 
 # Get path to our CSV file
 csv_path = "G:/Team Drives/princeton_gerrymandering_project/mapping/VA/Precinct Shapefile Collection/Transfer CSVs/SHP_transfer_all.csv"
@@ -26,19 +11,15 @@ csv_path = "G:/Team Drives/princeton_gerrymandering_project/mapping/VA/Precinct 
 # results of the transfer
 try:
     # Import Google Drive path
-    with open(csv_path) as f:
-        reader = csv.reader(f)
-        data = [r for r in reader]
-    drive_path = data[0][1]
+    direc_path = ht.read_one_csv_elem(csv_path)
     
-    # Import table from CSV into pandas dataframe
-    in_df = pd.read_csv(csv_path, header=1, names=['Folder', 'Source', \
-                                                   'Single File', 'Path'])
-    
-    # Initialize out_df, which contains the results of the transfers and 
-    # contains what will be copied into the conversion page of the Google
-    # sheet
-    new_cols = ['Result', 'Locality', 'File ix', 'Original Name', 'File Type', 
+    # Import table from CSV into pandas df
+    csv_col = ['Folder', 'Source', 'Single File', 'Path']
+    csv_list = []
+    csv_df = ht.read_csv_to_df(csv_path, 1, csv_col, csv_list)
+
+    # Initialize out_df, which contains the batching output
+    new_cols = ['Result', 'Locality', 'File ix', 'Original Name', 'File Type',\
                'Origin', 'Modified Name']
     out_df = pd.DataFrame(columns=new_cols)
     
@@ -46,21 +27,21 @@ try:
     no_entry_ext = ['cpg', 'dbf', 'prj', 'xml', 'shx', 'sbn', 'sbx', 'lock']
     
     # Iterate through each row/folder we are transferring files form
-    for i, _ in in_df.iterrows():
+    for i, _ in csv_df.iterrows():
 
         # Create Check that determines whether any files exist
         no_files_exist = True
         
         # Path to single file case. Single File will equal 1
-        if in_df.at[i, 'Single File']:
+        if csv_df.at[i, 'Single File']:
             
-            # Note that a file exists
+            # Boolean to determine if
             no_files_exist = False
             
             # Identify current and new full path names
-            file = in_df.at[i, 'Path'].split('/')[-1]
+            file = csv_df.at[i, 'Path'].split('/')[-1]
             file_type = file.split('.')[-1]                            
-            new_name = 'source_' + in_df.at[i, 'Source'] + \
+            new_name = 'source_' + csv_df.at[i, 'Source'] + \
                         '_old-name_' + file
             # ensure no spaces in filename
             new_name = new_name.replace(' ', '-')
@@ -73,8 +54,8 @@ try:
                 new_name = new_name.split('.')[0][:-len_delete] + '.' + ext
                 
             # Get current paths
-            cur_path = in_df.at[i, 'Path']
-            new_path = drive_path + '/' + in_df.at[i, 'Folder'] + '/' + \
+            cur_path = csv_df.at[i, 'Path']
+            new_path = direc_path + '/' + csv_df.at[i, 'Folder'] + '/' + \
                         new_name
                         
             # Try to copy file, otherwise print error in results csv and
@@ -86,8 +67,8 @@ try:
                 # Set out_df if extension is not in no_entry list
                 if file_type not in no_entry_ext:
                     row = len(out_df)
-                    out_df.at[row, 'Locality'] = in_df.at[i, 'Folder']
-                    out_df.at[row, 'Origin'] = in_df.at[i, 'Source']
+                    out_df.at[row, 'Locality'] = csv_df.at[i, 'Folder']
+                    out_df.at[row, 'Origin'] = csv_df.at[i, 'Source']
                     out_df.at[row, 'File ix'] = -1
                     out_df.at[row, 'File Type'] = file_type.upper()
                     out_df.at[row, 'Modified Name'] = new_name
@@ -102,8 +83,8 @@ try:
                 fail_str = 'FAILURE to TRANSFER: ' + error_str
                 print(error_str)
                 row = len(out_df)
-                out_df.at[row, 'Locality'] = in_df.at[i, 'Folder']
-                out_df.at[row, 'Origin'] = in_df.at[i, 'Source']
+                out_df.at[row, 'Locality'] = csv_df.at[i, 'Folder']
+                out_df.at[row, 'Origin'] = csv_df.at[i, 'Source']
                 out_df.at[row, 'File ix'] = -1
                 out_df.at[row, 'File Type'] = file_type.upper()
                 out_df.at[row, 'Modified Name'] = new_name
@@ -112,7 +93,7 @@ try:
                 
         else:
             # Get the files in the current folder
-            for _, _, files in os.walk(in_df.at[i, 'Path']):
+            for _, _, files in os.walk(csv_df.at[i, 'Path']):
                 
                 # Note that files exist in the folder, 
                 no_files_exist = False
@@ -122,7 +103,7 @@ try:
                     
                     # Identify current and new full path names
                     file_type = file.split('.')[-1]                            
-                    new_name = 'source_' + in_df.at[i, 'Source'] + \
+                    new_name = 'source_' + csv_df.at[i, 'Source'] + \
                                 '_old-name_' + file
                     # ensure no spaces in filename
                     new_name = new_name.replace(' ', '-')
@@ -135,8 +116,8 @@ try:
                         new_name = new_name.split('.')[0][:-len_delete] + '.' + ext
                     
                     # Get current paths
-                    cur_path = in_df.at[i, 'Path'] + '/' + file
-                    new_path = drive_path + '/' + in_df.at[i, 'Folder'] + '/' + \
+                    cur_path = csv_df.at[i, 'Path'] + '/' + file
+                    new_path = direc_path + '/' + csv_df.at[i, 'Folder'] + '/' + \
                                 new_name
                                 
                     # Try to copy file, otherwise print error in results csv and
@@ -148,8 +129,8 @@ try:
                         # Set out_df if extension is not in no_entry list
                         if file_type not in no_entry_ext:
                             row = len(out_df)
-                            out_df.at[row, 'Locality'] = in_df.at[i, 'Folder']
-                            out_df.at[row, 'Origin'] = in_df.at[i, 'Source']
+                            out_df.at[row, 'Locality'] = csv_df.at[i, 'Folder']
+                            out_df.at[row, 'Origin'] = csv_df.at[i, 'Source']
                             out_df.at[row, 'File ix'] = -1
                             out_df.at[row, 'File Type'] = file_type.upper()
                             out_df.at[row, 'Modified Name'] = new_name
@@ -164,8 +145,8 @@ try:
                         fail_str = 'FAILURE to TRANSFER: ' + error_str
                         print(error_str)
                         row = len(out_df)
-                        out_df.at[row, 'Locality'] = in_df.at[i, 'Folder']
-                        out_df.at[row, 'Origin'] = in_df.at[i, 'Source']
+                        out_df.at[row, 'Locality'] = csv_df.at[i, 'Folder']
+                        out_df.at[row, 'Origin'] = csv_df.at[i, 'Source']
                         out_df.at[row, 'File ix'] = -1
                         out_df.at[row, 'File Type'] = file_type.upper()
                         out_df.at[row, 'Modified Name'] = new_name
@@ -176,7 +157,7 @@ try:
         if no_files_exist:
             error_str = 'FOLDER ERROR: Either (1) No files exist in the ' \
                         'folder or (2) the following path is incorrect: ' + \
-                        in_df.at[i, 'Path'] + '\n'
+                        csv_df.at[i, 'Path'] + '\n'
             fail_str = 'FAILURE to TRANSFER:' + error_str
             print(error_str)
             row = len(out_df)
