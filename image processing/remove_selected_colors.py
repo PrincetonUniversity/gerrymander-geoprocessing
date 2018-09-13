753b4c2fefc5dbba1c63b588d966cd54ba78769d
Connor <connormoffatt4@gmail.com>
2018-09-13 13:47:53 -0400

    Finish remove selected colors

--- image processing/remove_selected_colors.py
+++ image processing/remove_selected_colors.py
@@ -8,15 +8,10 @@ from tqdm import tqdm
 img_path = ''
 img_path_out = ''
 
-#################
-img_path_amherst = "G:/Team Drives/princeton_gerrymandering_project/mapping/VA/Precinct Shapefile Collection/Virginia precincts/Amherst County/Amherst Screenshot Cropped.png"
-img_path = img_path_amherst
-img_path_out = './Test_img3.png'
-################
 # Do you want to reduce the number of colors in the current image 
 # (HIGHLY RECOMMENDED TO SET TO A VALUE. Set to any natural number if you want
 # to reduce. Set to 0 if you do NOT want to reduce)
-reduce_colors = 5
+reduce_colors = 8
 
 # Do you convert all non-white colors to black?
 convert_non_white_to_black = False
@@ -64,41 +59,57 @@ for y in tqdm(range(img.size[0])):
         for remove_color in remove_colors:
             if list(img_arr[x][y]) == list(remove_color):
                 remove = True
-                
-        # If it is a remove color
-        if remove and convert_non_white_to_black:
-            img_arr[x][y] = black
-        elif not remove:
-            img_arr[x][y] = white
+            
+        # Try to find other pixel to the right
+        if remove:
+            # iterate to the right until you find a different color
+            shift = x + 1
+            while shift < img.size[1]:
+                if tuple(img_arr[shift][y]) in remove_colors:
+                    shift += 1
+                else:
 
+                    img_arr[x][y] = img_arr[shift][y]
+                    shift = img.size[1]
+                    remove = False
+            
+            
+        # try to find other pixel to the left if one is not found to the right
+        if remove:
+            # iterate to the left until you find another color
+            shift = x - 1
+            while shift > 0:
+                if tuple(img_arr[shift][y]) in remove_colors:
+                    shift -= 1
+                else:
+                    img_arr[x][y] = img_arr[shift][y]
+                    shift = 0
+                    remove = False
+        
+        # try to find other pixel down if one has not been found
+        if remove:
+            # iterate down until you find another color
+            shift = y + 1
+            while shift < img.size[0]:
+                if tuple(img_arr[x][shift]) in remove_colors:
+                    shift += 1
+                else:
+                    img_arr[x][y] = img_arr[x][shift]
+                    shift = img.size[0]
+                    remove = False
+            
+        
+        # try to find other pixel up if one has not been found
+        if remove:
+            # iterate up until you find another color
+            shift = y - 1
+            while shift > 0:
+                if tuple(img_arr[x][shift]) in remove_colors:
+                    shift -= 1
+                else:
+                    img_arr[x][y] = img_arr[x][shift]
+                    shift = 0
 
 final_img = Image.fromarray(img_arr)
 final_img.save(img_path_out)
-
-
-
-#######################################################################3
-for y in range(back_conv_img.size[0]):
-    for x in range(back_conv_img.size[1]):
-        
-        #  Check if it a boundary color. Boundary color is saved
-        boundary = False
-        for boundary_color in boundary_colors:
-            if list(back_arr[x][y]) == list(boundary_color):
-                boundary = True
-                
-                
-                
-        # Change color of pixel
-        if boundary:
-            # iterate to the right until you find a different color
-            shift_x = x + 1
-            while shift_x < back_conv_img.size[1]:
-                if tuple(back_arr[shift_x][y]) in boundary_colors:
-                    shift_x += 1
-                else:
-                    back_arr[x][y] = back_arr[shift_x][y]
-                    shift_x = back_conv_img.size[1]
-
-
 
