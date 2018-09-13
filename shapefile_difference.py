cfa1e8765b9fd0e1e9820337bf3e227217757220
Connor <connormoffatt4@gmail.com>
2018-09-13 13:18:51 -0400

    Delete sections

--- shapefile_difference.py
+++ shapefile_difference.py
@@ -5,7 +5,7 @@ import helper_tools as ht
 
 # Get path to our CSV file
 csv_path = "G:/Team Drives/princeton_gerrymandering_project/mapping/VA/Precinct Shapefile Collection/CSV/Misc CSV/shapefile_difference_Warren_County_09_13_18.csv"
-#%%
+
 # Initial try and except to catch improper csv_path or error exporting the
 # results of the difference
 try:
