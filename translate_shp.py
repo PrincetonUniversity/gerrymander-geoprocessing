3e58c2eed2fcde5391f393b3d6a3373833837bf3
Connor <connormoffatt4@gmail.com>
2018-09-14 16:54:01 -0400

    Finish Translate

--- translate_shp.py
+++ translate_shp.py
@@ -2,9 +2,11 @@ import pandas as pd
 import geopandas as gpd
 import shapely as shp
 import helper_tools as ht
+from shapely.geometry import Polygon
+from shapely.geometry import LinearRing
 
 # Get path to our CSV file
-csv_path = "G:/Team Drives/princeton_gerrymandering_project/mapping/VA/Precinct Shapefile Collection/CSV/Misc CSV/shapefile_difference_VA_09_13_18.csv"
+csv_path = "G:/Team Drives/princeton_gerrymandering_project/mapping/VA/Precinct Shapefile Collection/CSV/Misc CSV/translate_shp_Roanoke_City_Sep_14.csv"
 
 # Initial try and except to catch improper csv_path or error exporting the
 # results of the difference
@@ -17,7 +19,7 @@ try:
     csv_df = ht.read_csv_to_df(csv_path, 1, csv_col, csv_list)
 
     # Initialize out_df, which contains the batching output
-    new_cols = ['Result', 'Locality Name', 'Percent Diff']
+    new_cols = ['Result', 'Locality Name']
     out_df = pd.DataFrame(columns=new_cols)
     
     # Iterate through each county we are finding the difference for
@@ -25,6 +27,10 @@ try:
         
         # Create geometry for entire locality
         try:
+            ######################
+            import time
+            start = time.time()
+            current = start
             # Define the locality
             local = csv_df.at[i, 'Locality Name']
             print(local)
@@ -60,15 +66,45 @@ try:
             polys2 = list(df2['geometry'])
             poly2 = shp.ops.cascaded_union(polys2)
             
-            # Find percent difference
-            diff = poly2.difference(poly1).area
-            perc_diff = diff / poly2.area
-        
-            # Place Results in out_df
-            row = len(out_df)
-            out_df.at[row, 'Result'] = 'SUCCESS'
-            out_df.at[row, 'Locality Name'] = csv_df.at[i, 'Locality Name']
-            out_df.at[row, 'Percent Diff'] = perc_diff
+            # Get the bounds for each 
+            bounds1 = poly1.bounds
+            bounds2 = poly2.bounds
+            
+            # Calculate shift x and shift y for second shapefile
+            shift_x = bounds2[0] - bounds1[0]
+            shift_y = bounds2[1] - bounds1[1]
+
+            # Iterate through every geometry in the second geodataframe
+            for j, _ in df1.iterrows():
+                
+                current = time.time()
+                # translate the polygon
+                poly = df1.at[j, 'geometry']
+                
+                # need to get the exterior
+                exterior = list(poly.exterior.coords)
+                new_ext = []
+                for c in exterior:
+                    c_new_x = c[0] + shift_x
+                    c_new_y = c[1] + shift_y
+                    new_ext.append((c_new_x, c_new_y))
+                    
+                # need to get the interiors
+                new_interiors = []
+                interiors = list(poly.interiors)
+                for inter in interiors:
+                    inter_coords = list(inter.coords)
+                    new_inter_coords = []
+                    for c in inter_coords:
+                        c_new_x = c[0] + shift_x
+                        c_new_y = c[1] + shift_y
+                        new_inter_coords.append((c_new_x, c_new_y))
+                        
+                    new_interiors.append(LinearRing(new_inter_coords))
+                        
+                df1.at[j, 'geometry'] = Polygon(new_ext, new_interiors)
+
+            ht.save_shapefile(df1, path_shape1)
         
         # Shapefile creation failed
         except:
@@ -76,7 +112,6 @@ try:
             row = len(out_df)
             out_df.at[row, 'Result'] = 'FAILURE'
             out_df.at[row, 'Locality Name'] = csv_df.at[i, 'Locality Name']
-            out_df.at[row, 'Percinct Diff'] = 'NA'
 
     # Create path to output our results CSV file and output
     csv_out_path = csv_path[:-4] + ' RESULTS.csv'
