0b5e6a41690d5de65bcd7cd905c6af5150f114e5
hwheelen <hwheelen@berkeley.edu>
2019-02-13 15:56:36 -0500

    scripts for working with voter roll

--- compare_shape_precincts.py
+++ compare_shape_precincts.py
@@ -15,12 +15,13 @@ warnings.filterwarnings("ignore")
 import helper_tools as ht
 
 
-counties = ['Vinton']
+counties = ['Adams']
+start_path = '/Volumes/GoogleDrive/Team Drives/princeton_gerrymandering_project/mapping/OH'
 
 # take 2 precint assignments for blocks and flag difference
-for cty in counties:
+for county in counties:
     # get census block shapefile with precinct assignments
-    shp_path = "/Users/jessicanyquist/Desktop/PGP/Vinton_County_census_block.shp"
+    shp_path = start_path + '/Precinct Data/' +county +' County/'+county+'_County_census_block_VR.shp'
 
     ht.delete_cpg(shp_path)
     df_shp = gpd.read_file(shp_path)
@@ -29,16 +30,16 @@ for cty in counties:
     df_shp['match'] = 0;
     
     col_1 = 'PREC_SHP'
-    col_2 = 'precinct_p'
+    col_2 = 'precinct'
     
     # check if precinct columns are the same 
     for i in range(0,len(df_shp.index)):
-        prec_1 = df_shp.at[i,col_1]
+        prec_1 = df_shp.at[i,col_1].upper()
         prec_2 = df_shp.at[i,col_2].upper()
         if(prec_1 != prec_2):
             df_shp.at[i, 'match'] = 1
             
-    result_path = '.Vinton_County_census_block_match.shp'
+    result_path = start_path + '/Precinct Data/' +county +' County/'+county+'_County_census_block_match.shp'
     ht.save_shapefile(df_shp, result_path)
         
         
